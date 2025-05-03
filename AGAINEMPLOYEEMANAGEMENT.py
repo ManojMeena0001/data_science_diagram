190c61aa4a1b69e67d483a7721ff575973c819d9
ManojMeena0001 <manojofficialmail689@gmail.com>
2025-05-03 18:12:32 +0530

    Add files via upload

---
+++ AGAINEMPLOYEEMANAGEMENT.py
@@ -0,0 +1,6 @@
+workers={'reception':['aditya','aarav','akhil','ankit','ansul'],
+         'management':['mohit','mehak','mehak','manas','mannat'],
+         'safety':['shaahid','shakeel','sonu','samar','shashank'],
+         'sales':['ramesh','mahesh','mukesh','ajit','atul'],
+         'quality':[
+         'manufacturing':
